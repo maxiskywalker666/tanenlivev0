dd26a30f98fd95ccce7bbd30c816d61f15624fcb
maxiskywalker666 <max812@free.fr>
2020-04-27 17:18:44 +0200

    New LookAndFeel class (Slider+Button)

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -12,12 +12,19 @@
 #include "PluginEditor.h"
 
 //==============================================================================
+
+
 TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0AudioProcessor& p)
     : AudioProcessorEditor (&p), processor (p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (500, 600);
+    setSize (500, 400);
+    byPassButton.setLookAndFeel(&reverbLook);
+    addAndMakeVisible(byPassButton);
+    //addAndMakeVisible(b2);
+    //addAndMakeVisible(b3);
+    
     auto& params = processor.getParameters();
     // COMBOBOX FILTER TYPE
     AudioParameterInt* filterTypeParameter = (AudioParameterInt*)params.getUnchecked(5);
@@ -38,6 +45,7 @@ TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0Aud
     mFilterCutoffSlider.setBounds(0, 0, 100, 100);
     mFilterCutoffSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mFilterCutoffSlider.setValue(*filterCutoffParameter);
+    mFilterCutoffSlider.setLookAndFeel(&filterLook);
     addAndMakeVisible(mFilterCutoffSlider);
     
     mFilterCutoffSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -50,6 +58,7 @@ TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0Aud
     mFilterResSlider.setBounds(0, 100, 100, 100);
     mFilterResSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mFilterResSlider.setValue(*filterResonanceParameter);
+    mFilterResSlider.setLookAndFeel(&filterLook);
     addAndMakeVisible(mFilterResSlider);
     
     mFilterResSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -63,6 +72,7 @@ TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0Aud
     mReverbDryWetSlider.setBounds(100, 0, 100, 100);
     mReverbDryWetSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mReverbDryWetSlider.setValue(*reverbDryWetParameter);
+    mReverbDryWetSlider.setLookAndFeel(&reverbLook);
     addAndMakeVisible(mReverbDryWetSlider);
     
     mReverbDryWetSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -76,6 +86,7 @@ TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0Aud
     mReverbRoomSizeSlider.setBounds(100, 100, 100, 100);
     mReverbRoomSizeSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mReverbRoomSizeSlider.setValue(*reverbRoomSizeParameter);
+    mReverbRoomSizeSlider.setLookAndFeel(&reverbLook);
     addAndMakeVisible(mReverbRoomSizeSlider);
     
     mReverbRoomSizeSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -89,6 +100,7 @@ TanenLiveV0AudioProcessorEditor::TanenLiveV0AudioProcessorEditor (TanenLiveV0Aud
     mReverbWidthSlider.setBounds(100, 200, 100, 100);
     mReverbWidthSlider.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     mReverbWidthSlider.setValue(*reverbWidthParameter);
+    mReverbWidthSlider.setLookAndFeel(&reverbLook);
     addAndMakeVisible(mReverbWidthSlider);
     
     mReverbWidthSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -109,11 +121,19 @@ void TanenLiveV0AudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("TANEN LIVE MACHINE", getLocalBounds(), Justification::centred, 1);
+    g.drawFittedText ("TANEN LIVE MACHINE", getLocalBounds(), Justification::centredBottom, 1);
 }
 
 void TanenLiveV0AudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    auto r = getLocalBounds();
+    auto topSection = r.removeFromTop(100);
+    //byPassButton.setBounds(topSection.removeFromLeft(100).reduced(10));
+    b2.setBounds(topSection.reduced(10));
+    b3.setBounds(r.reduced(10));
+    
+    byPassButton.setBounds(100, 310, 100, 30);
 }
+
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,6 +14,112 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
+class FilterLookAndFeel : public LookAndFeel_V4
+{
+public:
+    FilterLookAndFeel(){}
+    void drawRotarySlider (Graphics& g, int x, int y, int width, int height, float sliderPos, const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider) override
+        {
+            auto radius = jmin (width / 2, height / 2) - 4.0f;
+            auto centreX = x + width  * 0.5f;
+            auto centreY = y + height * 0.5f;
+            auto rx = centreX - radius;
+            auto ry = centreY - radius;
+            auto rw = radius * 2.0f;
+            auto angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+            
+            // fill
+            g.setColour (Colours::grey);
+            g.fillEllipse (rx, ry, rw, rw);
+            // outline
+            g.setColour (Colours::black);
+            g.drawEllipse (rx, ry, rw, rw, 3.0f);
+            // pointer
+            Path p;
+            auto pointerLength = radius * 0.53f;
+            auto pointerThickness = 4.0f;
+            p.addRectangle (-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+            p.applyTransform (AffineTransform::rotation (angle).translated (centreX, centreY));
+            // pointer
+            g.setColour (Colours::black);
+            g.fillPath (p);
+        }
+};
+
+class ReverbLookAndFeel : public LookAndFeel_V4
+{
+public:
+    ReverbLookAndFeel(){}
+    ~ReverbLookAndFeel(){
+        
+    }
+    void drawRotarySlider (Graphics& g, int x, int y, int width, int height, float sliderPos, const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider) override
+        {
+            auto radius = jmin (width / 2, height / 2) - 4.0f;
+            auto centreX = x + width  * 0.5f;
+            auto centreY = y + height * 0.5f;
+            auto rx = centreX - radius;
+            auto ry = centreY - radius;
+            auto rw = radius * 2.0f;
+            auto angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+            
+            // fill
+            g.setColour (Colours::lightsteelblue);
+            g.fillEllipse (rx, ry, rw, rw);
+            // outline
+            g.setColour (Colours::black);
+            g.drawEllipse (rx, ry, rw, rw, 3.0f);
+            // pointer
+            Path p;
+            auto pointerLength = radius * 0.53f;
+            auto pointerThickness = 4.0f;
+            p.addRectangle (-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+            p.applyTransform (AffineTransform::rotation (angle).translated (centreX, centreY));
+            // pointer
+            g.setColour (Colours::black);
+            g.fillPath (p);
+        }
+    void drawButtonBackground (Graphics& g, Button& button, const Colour& backgroundColour, bool isMouseOverButton, bool isButtonDown) override
+        {
+            auto buttonArea = button.getLocalBounds();
+            auto edge = 4;
+            buttonArea.removeFromLeft (edge);
+            buttonArea.removeFromTop (edge);
+            const Colour& colour = isMouseOverButton ? backgroundColour.withAlpha(0.8f) : backgroundColour;
+            // shadow
+            g.setColour (Colours::darkgrey.withAlpha (0.5f));
+            g.fillRect (buttonArea);
+            auto offset = isButtonDown ? -edge / 2 : -edge;
+            buttonArea.translate (offset, offset);
+            g.setColour (colour);
+            g.fillRect (buttonArea);
+
+        }
+    void drawButtonText (Graphics& g, TextButton& button, bool isMouseOverButton, bool isButtonDown) override
+        {
+            auto font = getTextButtonFont (button, button.getHeight());
+            g.setFont (font);
+            g.setColour (button.findColour (button.getToggleState() ? TextButton::textColourOnId
+                                                                    : TextButton::textColourOffId)
+                               .withMultipliedAlpha (button.isEnabled() ? 1.0f : 0.5f));
+         
+            auto yIndent = jmin (4, button.proportionOfHeight (0.3f));
+            auto cornerSize = jmin (button.getHeight(), button.getWidth()) / 2;
+         
+            auto fontHeight = roundToInt (font.getHeight() * 0.6f);
+            auto leftIndent  = jmin (fontHeight, 2 + cornerSize / (button.isConnectedOnLeft()  ? 4 : 2));
+            auto rightIndent = jmin (fontHeight, 2 + cornerSize / (button.isConnectedOnRight() ? 4 : 2));
+            auto textWidth = button.getWidth() - leftIndent - rightIndent;
+ 
+            auto edge = 4;
+            auto offset = isButtonDown ? edge / 2 : 0;
+            if (textWidth > 0)
+                g.drawFittedText (button.getButtonText(),
+                                  leftIndent + offset, yIndent + offset, textWidth, button.getHeight() - yIndent * 2 - edge,
+                                  Justification::centred, 2);
+        }
+};
+
 /**
 */
 class TanenLiveV0AudioProcessorEditor  : public AudioProcessorEditor
@@ -31,8 +137,10 @@ private:
     // access the processor object that created it.
     TanenLiveV0AudioProcessor& processor;
 
+    FilterLookAndFeel filterLook;
+    ReverbLookAndFeel reverbLook;
     // TANEN LIVE General Parameters
-    
+    TextButton byPassButton{"BYPASS"}, b2{"2"}, b3{"3"};
     // FILTER Parameters
     Slider mFilterCutoffSlider; // OK - log scale to do
     Slider mFilterResSlider; // OK
